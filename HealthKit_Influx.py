dd631d450ae2105c6e5d4cfe649a884cdc6f9a73
Evs91 <edamelio@gmail.com>
2018-03-01 11:46:26 -0500

    Updated variables

--- HealthKit_Influx.py
+++ HealthKit_Influx.py
@@ -3,13 +3,14 @@ from influxdb import InfluxDBClient
 
 #variables
 IP = ''
-username = ''
-password = ''
-database_name = 'Healthkit'
+USERNAME = ''
+PASSWORD = ''
+DB_NAME = 'Healthkit'
+NAME = 'MyNameHere'
 
 #setup influx client
-client = InfluxDBClient(IP, 8086, username, password, database_name)
-client.create_database(database_name)
+client = InfluxDBClient(IP, 8086, USERNAME, PASSWORD, DB_NAME)
+client.create_database(DB_NAME)
 
 #parse xml and add to influx via API
 xmldoc = minidom.parse('export.xml')
@@ -18,7 +19,7 @@ for s in recordlist:
     if s.attributes['type'].value == "HKQuantityTypeIdentifierHeartRate":
         HealthKitTimeStamp = s.attributes['startDate'].value
         AppleWatchHRValue = s.attributes['value'].value
-        json_body = [{"measurement": "heartrate","tags": {"service": "HealthKit","person": "Everett"},"time": HealthKitTimeStamp,"fields": {"watch_heartrate": float(AppleWatchHRValue)}}]
+        json_body = [{"measurement": "heartrate","tags": {"service": "HealthKit","person": NAME},"time": HealthKitTimeStamp,"fields": {"watch_heartrate": float(AppleWatchHRValue)}}]
         client.write_points(json_body)
     else:
         pass
