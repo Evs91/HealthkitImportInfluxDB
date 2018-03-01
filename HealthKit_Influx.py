efb98724dcba5ed9a544e5e57a470076b27f17f6
Evs91 <edamelio@gmail.com>
2018-03-01 11:39:22 -0500

    Update 1 - add script

---
+++ HealthKit_Influx.py
@@ -0,0 +1,24 @@
+from xml.dom import minidom
+from influxdb import InfluxDBClient
+
+#variables
+IP = ''
+username = ''
+password = ''
+database_name = 'Healthkit'
+
+#setup influx client
+client = InfluxDBClient(IP, 8086, username, password, database_name)
+client.create_database(database_name)
+
+#parse xml
+xmldoc = minidom.parse('export.xml')
+recordlist = xmldoc.getElementsByTagName('Record')
+for s in recordlist:
+    if s.attributes['type'].value == "HKQuantityTypeIdentifierHeartRate":
+        HealthKitTimeStamp = s.attributes['startDate'].value
+        AppleWatchHRValue = s.attributes['value'].value
+        json_body = [{"measurement": "heartrate","tags": {"service": "HealthKit","person": "Everett"},"time": HealthKitTimeStamp,"fields": {"watch_heartrate": float(AppleWatchHRValue)}}]
+        client.write_points(json_body)
+    else:
+        pass
